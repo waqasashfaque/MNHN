23c2ef1eee8cd4272887f24424893dca0f5203e2
Waqas Ashfaque <153775325+waqasashfaque@users.noreply.github.com>
2025-07-26 03:36:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import re
-import plotly.express as px
+import plotly as px
 
 
 st.set_page_config(layout="wide", page_title="MNHN Dashboard")
